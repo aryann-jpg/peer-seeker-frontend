06771b51f577f95ec67cf37a22266ec4da0e6cd5
aryan <2401568G@student.tp.edu.sg>
2026-01-14 18:40:19 +0800

    Fix file casing for Vercel build

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router";
-import "../css/login.css";
+import "../css/Login.css";
 
 const Login = () => {
   const navigate = useNavigate();
